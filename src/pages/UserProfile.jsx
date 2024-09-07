6f451124839afd6c1c7237679d092731ab5484c0
Devansh <devanshpratap1500@gmail.com>
2024-09-07 17:02:16 +0530

    added some changes

---
+++ src/pages/UserProfile.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Card from '../components/profile/Card'
+
+const UserProfile = () => {
+  return (
+    <div>
+      <Card />
+      <div>hello</div>
+    </div>
+  )
+}
+
+export default UserProfile
